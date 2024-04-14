31e24f4d9aae8c1795fee47cd467fef04481f463
Becker-RS <115456808+Becker-RS@users.noreply.github.com>
2024-04-14 07:42:33 -0300

    Botão 1,2,3

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ cor4 = "#ECEFF1" #cinza
 cor5 = "#FFAB40" #laranja
 
 
-
-
 janela =Tk()
 janela.title("Calculadora")
 janela.geometry("235x318")
@@ -22,5 +20,13 @@ Frame_tela.grid(row=0, column=0)
 Frame_base = Frame(janela, width=235, height=268,)
 Frame_base.grid(row=1, column=0)
 
+b_1 = Button(Frame_base, text="C", width=11,height=2)
+b_1.place(x=0, y=0)
+
+b_2 = Button(Frame_base, text="%", width=5,height=2)
+b_2.place(x=90, y=0)
+
+b_3 = Button(Frame_base, text="/", width=5,height=2)
+b_3.place(x=177, y=0)
 
 janela.mainloop()

061d2ab9c1f3f18356e8de25011dec0980bd3e8d
Becker-RS <115456808+Becker-RS@users.noreply.github.com>
2024-04-14 07:32:39 -0300

    estrutura definida

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ cor1 = "#1e1f1e" #preto
 cor2 = "#feffff" #branco
 cor3 = "#38576b" #azul
 cor4 = "#ECEFF1" #cinza
-cor5 = "#FFAB40" #Laranja
+cor5 = "#FFAB40" #laranja
 
 
 
@@ -16,7 +16,11 @@ janela.geometry("235x318")
 janela.config(bg=cor1)
 
 
-Frame_tela = Frame(janela, width=235, height=50,)
+Frame_tela = Frame(janela, width=235, height=50,bg= cor3)
 Frame_tela.grid(row=0, column=0)
+
+Frame_base = Frame(janela, width=235, height=268,)
+Frame_base.grid(row=1, column=0)
+
 
 janela.mainloop()

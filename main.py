60159f9466d85b9c645d42538e41e02163afeb60
Becker-RS <115456808+Becker-RS@users.noreply.github.com>
2024-04-14 07:54:51 -0300

    Segunda linha de Botões

--- main.py
+++ main.py
@@ -29,4 +29,17 @@ b_2.place(x=118, y=0)
 b_3 = Button(Frame_base, text="/", width=5,height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_3.place(x=177, y=0)
 
+
+b_4 = Button(Frame_base, text="7", width=5,height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE) 
+b_4.place(x=0, y=52)
+
+b_5 = Button(Frame_base, text="8", width=5,height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_5.place(x=59, y=52)
+
+b_6 = Button(Frame_base, text="9", width=5,height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_6.place(x=118, y=52)
+
+b_7 = Button(Frame_base, text="*", width=5,height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_7.place(x=177, y=52)
+
 janela.mainloop()
